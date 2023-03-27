04f765d7a36f610489c8cc1e615ac32741f152e4
Asra Ali <asraa@google.com>
2023-03-27 11:23:26 -0500

    fix includes
    Signed-off-by: Asra Ali <asraa@google.com>

--- lib/Hello/LowerToArith.cpp
+++ lib/Hello/LowerToArith.cpp
@@ -17,6 +17,7 @@
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 
 #include "mlir/Pass/Pass.h"
