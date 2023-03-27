d81d30ebd95d66eead24581ab27ed0ab5e51c6c1
Asra Ali <asraa@google.com>
2023-03-27 11:18:33 -0500

    Add legal dialects
    Signed-off-by: Asra Ali <asraa@google.com>

--- lib/Hello/LowerToArith.cpp
+++ lib/Hello/LowerToArith.cpp
@@ -57,6 +57,8 @@ void MemRefGlobalToArithLoweringPass::runOnOperation() {
   mlir::ConversionTarget target(getContext());
   
   // Not sure if I should also add mlir::memref::GlobalOp as illegal and remove that.
+  target.addLegalDialect<mlir::AffineDialect, mlir::BuiltinDialect,
+    mlir::func::FuncDialect, mlir::arith::ArithDialect, mlir::memref::MemRefDialect>();
   target.addIllegalOp<mlir::memref::GetGlobalOp>();
   target.addLegalOp<mlir::arith::ConstantOp, mlir::memref::AllocOp, mlir::AffineStoreOp>();
 
